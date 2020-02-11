0969e9c9b172b0b403562d675baacff5766a982b
Andy Hayes <andrew.hayes@anno.ai>
2020-02-11 13:49:30 -0600

    cleanup

--- app.js
+++ app.js
@@ -2,7 +2,5 @@ const OpenFaasWrapper = require('./index')
     
 const faas = new OpenFaasWrapper('http://localhost:8080')
 
-faas.call('openalpr', 'https://cdn.images.express.co.uk/img/dynamic/24/590x/DVLA-number-plates-2017-67-new-car-847566.jpg')
-    .then((data) => {
-        console.log(data)
-    })
+const res = faas.call('functionName', 'https://website.com/image.png')
+console.log(res)
