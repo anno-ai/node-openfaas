6174d032533e0acd6db1afd8136f5aff641cdd06
Andy Hayes <andrew.hayes@anno.ai>
2020-02-11 12:27:51 -0600

    cleanup

--- wrapper/index.js
+++ wrapper/index.js
@@ -7,9 +7,15 @@ class OpenFaasWrapper {
         this.provider = provider
     }
 
-    call = (functionName, url, config = { method: 'POST' }) => { // isJson: true, isBinaryResponse: false
+    /**
+     * function to execute an openfaas function held at the provider location
+     * @param {string} functionName - the name of the function
+     * @param {string} params - the query string or url to pass as request body
+     * @param {object} config - http request configuration 
+     */
+    call = (functionName, params, config = { method: 'POST' }) => { // isJson: true, isBinaryResponse: false
         const funcPath = path.join('/function', functionName)
-        config.body = url
+        config.body = params
         config.encoding = (config.isBinaryResponse ? null : 'utf8')
         console.log(config)
         fetch(this.provider + funcPath, config)
@@ -18,6 +24,7 @@ class OpenFaasWrapper {
             })
             .then((json) => {
                 console.log(json)
+                return json
             })
             .catch((e) => {
                 console.log('something went wrong: ', e)
